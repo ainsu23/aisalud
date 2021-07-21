f8cf4ee4e30be481393413221de4491f8201d8bf
Andres Insuasty <afinsuasty@hotmail.com>
2021-07-21 09:49:45 -0500

    docs: Corrige documentacion a funcion descriptiva
    Adelanto #B22

--- R/descriptiva.R
+++ R/descriptiva.R
@@ -14,24 +14,25 @@
 #
 
 #' @title Información descriptiva
-#' @description Calcula información descriptiva de las columnas seleccionadas
-#' @param data Tabla cargada a Análitica integrada
-#' @param columnas agrupador o agrupadaroes para realizar el análisis 
-#' descriptivo
-#' @param columna_valor columna númerica que contiene valores de calculo
-#' @param columna_suma Columna sobre la cual se realiza el análisis
-#' @param prestaciones boolean que define la frecuencia por prestación
-#' @param frec_cantidad boolean que define si es conteo o suma de valores de 
-#' columna_suma
-#' @return lista de tablas con información descriptiva y calculo de valores
-
-
+#' @description Calcula información descriptiva sobre el valor de las columnas 
+#' seleccionadas.
+#' @param data Tabla cargada a Análitica integrada.
+#' @param columnas Agrupador o agrupadores para realizar el análisis.
+#' descriptivo.
+#' @param columna_valor Columna númerica que contiene valores de calculo.
+#' @param columna_suma Columna referente a la unidad de conteo de los 
+#' agrupadores.
+#' @param prestaciones Boolean que define la frecuencia por prestación.
+#' @param frec_cantidad Boolean que define si la frecuencia se realiza haciendo 
+#' el conteo de registros o  sumando la columna cantidad.
+#' @return Lista con tabla descriptiva y tabla resumen con la suma de la columna
+#' *columna_valor* agrupada por *columa_suma* y *columnas*
 
 
 
 descriptiva <- function(data, columnas, columna_valor, columna_suma,
                         prestaciones = FALSE, frec_cantidad = FALSE) {
-
+  
   unidad <- "Episodio"
   if (prestaciones) unidad <- "Prestación"
   if (columna_suma == "nro_factura") unidad <- "Factura"
